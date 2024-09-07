938dc424defc9171157194873c4c888b8be96039
Plyska Oleksandr <oplyska@gmail.com>
2024-09-08 02:13:28 +0300

    first commit

---
+++ catalog/language/de-de/common/cart.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_items']     = '%s - %s';
+$_['text_empty']     = 'Ihr Warenkorb ist leer!';
+$_['text_cart']      = 'Warenkorb ansehen';
+$_['text_checkout']  = 'Auschecken';
+$_['text_recurring'] = 'Zahlungsprofil';
